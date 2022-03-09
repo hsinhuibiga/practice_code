b4eff3c4a824c33737449936f0901ba7728e6b50
hsinhuibiga <hsinhuibiga@gmail.com>
2022-03-09 18:01:46 +0800

    Add files via upload

---
+++ remove-duplicates-from-sorted-list-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+		root = ListNode(0)
+        root.next = head
+        val_list = []
+        while head:
+            val_list.append(head.val)
+            head = head.next
+        counter = collections.Counter(val_list)
+        head = root
+        while head and head.next:
+            if counter[head.next.val] != 1:
+                head.next = head.next.next
+            else:
+                head = head.next
+        return root.next
